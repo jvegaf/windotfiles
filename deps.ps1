7fbb3dd3ebf7602467c523dde7239cc112ad64ee
Jose Vega <josevega234@gmail.com>
2022-03-07 20:58:14 +0100

    add confs
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- deps.ps1
+++ deps.ps1
@@ -24,55 +24,19 @@ if ((which cinst) -eq $null) {
     choco feature enable -n=allowGlobalConfirmation
 }
 
-choco install -y chocolatey-core.extension
-
-# node-lts
-choco install nodejs-lts                        --limit-output
-
-# system and cli
-choco install curl                              --limit-output
-choco install nuget.commandline                 --limit-output
-choco install webpi                             --limit-output
-choco install git                               --limit-output
-choco install python                            --limit-output
-choco install ruby                              --limit-output
-choco install sysinternals                      --limit-output
-
-#fonts
-choco install sourcecodepro                     --limit-output
-
-# browsers
-choco install Opera                             --limit-output; <# pin; evergreen #> choco pin add --name Opera               --limit-output
-
-# dev tools and frameworks
-choco install dotnet-sdk                        --limit-output
-choco install neovim --pre                      --limit-output
-
-choco install vscode                            --limit-output
-choco install vscode-docker                     --limit-output
-
-choco install postman                           --limit-output
-
-choco install azurepowershell                   --limit-output
-choco install microsoftazurestorageexplorer     --limit-output
-
-choco install visualstudio2019professional      --limit-output
-
 ### Windows Features
 Write-Host "Installing Windows Features..." -ForegroundColor "Yellow"
 Enable-WindowsOptionalFeature -Online -FeatureName containers -All -NoRestart | Out-Null
 
 Refresh-Environment
 
-gem pristine --all --env-shebang
-
 ### Node Packages
-Write-Host "Installing Node Packages..." -ForegroundColor "Yellow"
-if (which npm) {
-    npm update npm
-    npm install -g gulp
-    npm install -g node-inspector
-    npm install -g @angular/cli
-    npm install -g @angular/language-server
-}
+# Write-Host "Installing Node Packages..." -ForegroundColor "Yellow"
+# if (which npm) {
+#     npm update npm
+#     npm install -g gulp
+#     npm install -g node-inspector
+#     npm install -g @angular/cli
+#     npm install -g @angular/language-server
+# }
 
---
+++ scripts/install_pkgs.ps1
@@ -0,0 +1,56 @@
+﻿#install Chocolatey when needed
+if (-not (Test-Path -Path "$env:ProgramData\Chocolatey\choco.exe" -PathType Leaf)) {
+    # from https://chocolatey.org/install
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Refresh-Environment
+    choco feature enable -n=allowGlobalConfirmation 
+}
+
+#install packages from file
+$chocoPkgsPath = "$env:HOMEPATH\.dotfiles\pkgs\choco\packages.config";
+
+choco install -y $chocoPkgsPath
+
+#Install WinGet
+#Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
+$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
+    "Installing winget Dependencies"
+    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri $releases_url
+    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+
+    "Installing winget from $($latestRelease.browser_download_url)"
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+else {
+    "winget already installed"
+}
+
+
+#Configure WinGet
+Write-Output "Configuring winget"
+
+#winget config path from: https://github.com/microsoft/winget-cli/blob/master/doc/Settings.md#file-location
+$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json";
+$settingsJson = 
+@"
+    {
+        // For documentation on these settings, see: https://aka.ms/winget-settings
+        "experimentalFeatures": {
+          "experimentalMSStore": true,
+        }
+    }
+"@;
+$settingsJson | Out-File $settingsPath -Encoding utf8
+
+#Install apps
+$wingetPkgsPath = "$env:HOMEPATH\.dotfiles\pkgs\winget\packages.json"; 
+ 
+winget import -i $wingetPkgsPath --accept-package-agreements --accept-source-agreements
